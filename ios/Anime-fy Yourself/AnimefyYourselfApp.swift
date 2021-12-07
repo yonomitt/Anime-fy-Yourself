0251239a0f777a399c519a8c5e7fe015d4e46d42
Yono Mittlefehldt <yono@toojuice.com>
2021-12-07 21:09:23 +0100

    File ▶︎ New ▶︎ Project...

---
+++ ios/Anime-fy Yourself/AnimefyYourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimefyYourselfApp.swift
+//  Anime-fy Yourself
+//
+//  Created by Yonatan Mittlefehldt on 2021-07-12.
+//
+
+import SwiftUI
+
+@main
+struct AnimefyYourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
